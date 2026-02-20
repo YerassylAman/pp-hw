324712e323f41ec7615ab762ed80f4cba341a78a
YerassylAman <amangeldiera9907@gmail.com>
2026-02-20 20:40:55 +0500

    added some files

--- Practice 3/Inheritance/super().py
+++ Practice 3/Inheritance/super().py
@@ -1,7 +1,17 @@
-class Student(Person):
-  def __init__(self, fname, lname, year):
-    super().__init__(fname, lname)
-    self.graduationyear = year
+class Person:
+  def __init__(self, fname, lname):
+    self.firstname = fname
+    self.lastname = lname
 
-  def welcome(self):
-    print("Welcome", self.firstname, self.lastname, "to the class of", self.graduationyear)
+  def printname(self):
+    print(self.firstname, self.lastname)
+
+#Use the Person class to create an object, and then execute the printname method:
+
+x = Person("John", "Doe")
+x.printname()
+class Student(Person):
+  def __init__(self, fname, lname):
+    super().__init__(fname, lname)
+
+    

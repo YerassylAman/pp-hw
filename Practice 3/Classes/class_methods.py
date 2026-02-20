de8ad27883e894244a1f50fa331dc3e08834ed67
YerassylAman <amangeldiera9907@gmail.com>
2026-02-20 20:32:33 +0500

    added some files

---
+++ Practice 3/Classes/class_methods.py
@@ -0,0 +1,43 @@
+class Person:
+  def __init__(self, name):
+    self.name = name
+
+  def greet(self):
+    print("Hello, my name is " + self.name)
+
+p1 = Person("Emil")
+p1.greet()
+
+
+class Calculator:
+  def add(self, a, b):
+    return a + b
+
+  def multiply(self, a, b):
+    return a * b
+
+calc = Calculator()
+print(calc.add(5, 3))
+print(calc.multiply(4, 7))
+
+class Person1:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+  def get_info(self):
+    return f"{self.name} is {self.age} years old"
+
+p2 = Person1("Tobias", 28)
+print(p2.get_info())
+
+class Person2:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+  def __str__(self):
+    return f"{self.name} ({self.age})"
+
+p3 = Person2("Tobias", 36)
+print(p3)

31b3bdb8f8a2450a56422d139a9321c81e810712
YerassylAman <amangeldiera9907@gmail.com>
2026-02-20 20:11:36 +0500

    added some files

---
+++ Practice 3/Functions/functions_arguments.py
@@ -0,0 +1,14 @@
+def my_function(fname):
+  print(fname + " Refsnes")
+
+my_function("Emil")
+my_function("Tobias")
+my_function("Linus")
+
+def my_function1(name = "friend"):
+  print("Hello", name)
+
+my_function1("Emil")
+my_function1("Tobias")
+my_function1()
+my_function1("Linus")
